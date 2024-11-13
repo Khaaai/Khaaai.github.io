55253c672b1a37311365d57bcf28dd15e62278e9
Khai <evelyne.tran@hotmail.fr>
2024-11-13 22:47:21 +0100

    add navbar + bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbCollapse, NgbNavLink, NgbNav, NgbNavItem } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbCollapse,NgbNav, NgbNavLink, NgbNavItem],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio';
+  isMenuCollapsed = true;
 }
